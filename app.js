7ed8430054b5d58810985aad13e600dff4cd15bf
Gitsaarju <080bct003@ioepc.edu.np>
2024-07-11 17:54:16 +0545

    Day 5

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const { name } = require("ejs")
 const express=require("express")
 const connectToDb = require("./database/databaseConnection")
 const Blog=require ("./model/blogmodel")
+const bcrypt = require('bcrypt')
+const register = require("./model/usermodel")
+// const login = require ("./model/usermodel")
 const encodeURI = require('encodeurl')
 const app = express()
 // const multer=require("./middleware/multerConfig").multer
 // const storge- require("./middleware/multerConfig").storage
 
 const{multer,storage} = require ('./middleware/multerConfig')
+const Register = require("./model/usermodel")
 const upload= multer({storage:storage})
 
 connectToDb()
@@ -87,14 +91,76 @@ app.get("/deleteblog/:id",async (req,res)=>{
     await Blog.findByIdAndDelete(id)
     res.redirect("/")
 })
-app.get("/editblog/:id",(req,res)=>{
-    res.render("./editBlog")
+app.get("/Updateblog/:id",async (req,res)=>{
+    const blog = req.params.id
+    await Blog.findByIdAndDelete(id)
+    const {id}=req.params
 
+    res.render("./Editblog",{blog:blog})
 
 })
+
+app.post("/Editblog/:id",async(req,res)=>{
+    const id=req.parms.id
+    const {title,subtitle,description} =req.body
+    await Blog.findByIdAndUpdate(id,{
+        title:title,
+        subtitle:subtitle,
+        description:description
+    })
+    res.redirect("/blogpage/" + id)
+})
+    
+
+app.get("/register",(req,res)=>{
+    res.render("./Authentication/register")
+})
+
+
+app.post("/register",async (req,res)=>{
+    const {username,email,password} = req.body 
+   await User.create({
+        username : username, 
+        email : email, 
+        password : bcrypt.hashSync(password,12)
+    })
+    // res.send("User registered successfully")
+    res.redirect("/login")
+})
+    
+    
+
+
+app.get("/login",(req,res)=>{
+    res.render("./Authentication/login")
+})
+
+app.post("/login",async(req,res)=>{
+   
+    const{email,password}=req.body
+    const data = await User.findOne({email:email})
+    if (user.length ===0){
+        res.send("Invalid email")
+        // res.redirect("/login")
+    }
+    else{
+        const isMatched=bcrypt.comparesync(password,user[0].password)
+        if(!isMatched){
+            res.send("Invalid password")
+            // res.redirect("/login")
+        } else{
+            res.send("Logged in successful")
+        }
+        }
+    })
+
+    
+
+   
     
 app.use(express.static("./storage"))
 app.listen(3000
     ,()=>{
-    console.log("Nodejs Project Day 1" + 3000)
+    console.log("Nodejs Project" + 3000)
+
 })
